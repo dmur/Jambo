ccd65d3520e6be725e750094995abccf5eb7b604
Dolan Murphy <admin@dmur.me>
2014-12-20 13:41:35 -0800

    Load the jam's background image from the JSON

--- Jambot/JamViewController.swift
+++ Jambot/JamViewController.swift
@@ -10,16 +10,22 @@ import UIKit
 import Alamofire
 
 class JamViewController: UIViewController {
+  
+    
+  @IBOutlet weak var jamBackgroundView: UIImageView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-      Alamofire.request(.GET, "http://api.thisismyjam.com1/dmur.json").response {
-        (request, response, data, error) in
-        println(request)
-        println(response)
-        println(data)
-        println(error)
+      Alamofire.request(.GET, "http://api.thisismyjam.com/1/dmur.json", parameters: nil, encoding: .JSON).responseJSON {
+        (_, _, profileData, _) in
+        let profile = profileData as NSDictionary,
+            jam = profile["jam"] as NSDictionary
+        let imageUrl = NSURL(string: jam["background"] as NSString)
+        var imageData = NSData(contentsOfURL: imageUrl!)
+        self.jamBackgroundView.image = UIImage(data: imageData!)
+        var img = UIImage(data: imageData!)
+        println(img!.size)
       }
         // Do any additional setup after loading the view.
     }
