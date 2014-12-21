7588ae20e2fcc69bbf17ffb98a927f8cedd529b7
Dolan Murphy <admin@dmur.me>
2014-12-20 19:28:14 -0800

    Added jam info labels currentJamIs, title, artist

--- Jambo/JamViewController.swift
+++ Jambo/JamViewController.swift
@@ -15,20 +15,27 @@ class JamViewController: UIViewController {
   @IBOutlet weak var jamBackgroundView: UIImageView!
   @IBOutlet weak var jamvatarView: UIImageView!
   @IBOutlet weak var jamInfoBackgroundView: UIImageView!
+  @IBOutlet weak var jamTitleLabel: UILabel!
+  @IBOutlet weak var jamArtistLabel: UILabel!
+  @IBOutlet weak var currentJamIsLabel: UILabel!
   
 
     override func viewDidLoad() {
         super.viewDidLoad()
+      
+      let username = "dmur"
 
-      Alamofire.request(.GET, "http://api.thisismyjam.com/1/dmur.json", parameters: nil, encoding: .JSON).responseJSON {
+      Alamofire.request(.GET, "http://api.thisismyjam.com/1/\(username).json", parameters: nil, encoding: .JSON).responseJSON {
         (_, _, profileData, _) in
         let profile = profileData as NSDictionary,
             jam = profile["jam"] as NSDictionary
         
+        self.currentJamIsLabel.text = "\(username)'s current jam is:"
         
         println(profile)
         self.loadJamBackground(jam)
         self.loadJamvatar(jam)
+        self.loadJamInfo(jam)
       }
         // Do any additional setup after loading the view.
     }
@@ -62,9 +69,16 @@ class JamViewController: UIViewController {
   
   func loadJamInfo(jam: NSDictionary) {
     let jamArtist = jam["artist"] as NSString,
-        jamTitle = jam["title"] as NSString
+        jamTitle = jam["title"] as NSString,
+        jamColor = UIColor(red: 0.3, green: 0.3, blue: 0.1, alpha: 1.0)
     
-    //self.jamTitleLabel.text = jamTitle
+    self.jamTitleLabel.text = jamTitle
+    self.jamTitleLabel.textColor = jamColor
+    
+    var artistString = NSMutableAttributedString(string: "by \(jamArtist)")
+    artistString.addAttribute(NSForegroundColorAttributeName, value: UIColor(white: 0.2, alpha: 1.0), range: NSMakeRange(0, 3))
+    artistString.addAttribute(NSForegroundColorAttributeName, value: jamColor, range: NSMakeRange(3, artistString.length - 3))
+    self.jamArtistLabel.attributedText = artistString
   }
 
     override func didReceiveMemoryWarning() {
