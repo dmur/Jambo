732445ab9fdb127926b09d5462db59a3bd22dc17
Dolan Murphy <admin@dmur.me>
2014-12-21 22:04:31 -0800

    Animating the first encounter

--- Jambo/JamboSupViewController.swift
+++ Jambo/JamboSupViewController.swift
@@ -10,11 +10,27 @@ import UIKit
 
 class JamboSupViewController: UIViewController {
 
+  @IBOutlet weak var jamboHeadWidthConstraint: NSLayoutConstraint!
+  @IBOutlet weak var messageLabel: UILabel!
+  @IBOutlet weak var jamUsernameField: UITextField!
+  @IBOutlet weak var jamUsernameButton: UIButton!
+  
+  
     override func viewDidLoad() {
         super.viewDidLoad()
-
+      self.showJambo()
         // Do any additional setup after loading the view.
     }
+  
+  func showJambo() {
+    self.jamboHeadWidthConstraint.constant = 150
+    self.view.setNeedsLayout()
+    UIView.animateWithDuration(0.5, delay: 1.0, usingSpringWithDamping: 0.8, initialSpringVelocity: 0, options: .CurveEaseIn, animations: ({ () -> Void in
+      self.view.layoutIfNeeded()
+    }), completion: ({ (_) -> Void in
+      self.messageLabel.hidden = false
+    }))
+  }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
