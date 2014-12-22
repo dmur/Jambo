c9c68837212d5e15a2c5e7d892eb40a41845e8ca
Dolan Murphy <admin@dmur.me>
2014-12-22 00:22:20 -0800

    Removing unnecessary boilerplate

--- Jambo/JamboSupViewController.swift
+++ Jambo/JamboSupViewController.swift
@@ -61,20 +61,5 @@ class JamboSupViewController: UIViewController, UITextFieldDelegate {
       jamViewController.username = self.jamUsernameField.text
     }
   }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
   
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
