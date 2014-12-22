b29acd11292267e398293f67be2240fbac7b0b30
Dolan Murphy <admin@dmur.me>
2014-12-21 21:29:43 -0800

    Added intro view controller

---
+++ Jambo/JamboSupViewController.swift
@@ -0,0 +1,35 @@
+//
+//  JamboSupViewController.swift
+//  Jambo
+//
+//  Created by Dolan Murphy on 12/21/14.
+//  Copyright (c) 2014 Dolan Murphy. All rights reserved.
+//
+
+import UIKit
+
+class JamboSupViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
